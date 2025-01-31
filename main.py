00f9dfcc806a46af9b878f6a1b086066ef757ba4
lunoLiv <156929085+lunoLiv@users.noreply.github.com>
2025-01-31 13:42:46 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+from openpyxl import load_workbook
 
+wb = load_workbook(filename='teste.xlsx')
+ws = wb.active
+
+gap = ws['L2':'L15074']
+
+for i in gap:
+    for j in i:
+        print(j.value)
+        input()
