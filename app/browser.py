3d541f0c314633c83d85c29c3799e01eabc0c7aa
lunoLiv <156929085+lunoLiv@users.noreply.github.com>
2025-01-31 08:45:03 -0300

    Update browser.py

--- app/browser.py
+++ app/browser.py
@@ -1,1 +1,6 @@
+def estado_boleto():
+  estado = driver.find_element(By.XPATH,'//*[@id="root"]/div/div[2]/div[2]/div[3]/div[2]/div/div[2]/div')
 
+  if estado.text == 'Quebra':
+    numParcela = driver.find_element(By.XPATH,'//*[@id="root"]/div/div[2]/div[2]/div[4]/div[1]/div[2]/div[3]/div[2]/p[2]').text
+    valorTotal = driver.find_element(By.XPATH,'//*[@id="root"]/div/div[2]/div[2]/div[4]/div[1]/div[2]/div[3]/div[5]/p[2]').text
