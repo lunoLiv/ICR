2ac552a06986dcee9bee2314cbabde34d32a41d3
lunoLiv <156929085+lunoLiv@users.noreply.github.com>
2025-02-07 13:05:29 -0300

    Create configs.py

---
+++ Web/Config/configs.py
@@ -0,0 +1,13 @@
+#Credenciais TMJ
+
+LOGIN = ''
+SENHA = ''
+
+#---------------------------------------------------
+#Numero de workers para divisao do df
+NUM_WORKERS = 5
+
+#---------------------------------------------------#
+#Caminho de entrada e saida no excel
+FILE_PATH_EXCEL_TO_READ = r'Database\leitura_repasses.xlsx'
+FILE_PATH_EXCEL_EXIT = r'cod_repasses.xlsx'
